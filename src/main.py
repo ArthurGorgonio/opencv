dc74130fec65bee16cec283566423889b722f6e1
ArthurGorgonio <gorgonioarthur@gmail.com>
2019-03-23 00:50:57 -0300

    comment the main function, testing Docstrings by PEP 257

--- src/main.py
+++ src/main.py
@@ -2,6 +2,15 @@ import cv2
 
 
 def main():
-    img = cv2.imread('data/by5y3.png', 0)
+    """
+    This function just open a specific image and write this in another dir
+    with orther name to test the basic of the opencv library
+
+    USAGE:
+        from main import main
+
+        main()
+    """
+    img = cv2.imread('samples/by5y3.png', 1)
     status = cv2.imwrite('./python_grey.png', img)
     print("Image written to file-system : ", status)
