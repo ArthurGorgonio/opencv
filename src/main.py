ff8acd3eaa048b2472b00a47832a713cbceaf98d
ArthurGorgonio <gorgonioarthur@gmail.com>
2019-03-27 22:56:16 -0300

    Testing the diferents functions to fix blur into images

--- src/main.py
+++ src/main.py
@@ -11,6 +11,24 @@ def main():
 
         main()
     """
-    img = cv2.imread('samples/by5y3.png', 1)
-    status = cv2.imwrite('./python_grey.png', img)
+    img = cv2.imread('samples/by5y3.png', 0)
+    status = 0
+    gauss = "./binary_gauss_"
+    thresh = "./binary_threshold_"
+    nogauss = "./binary_nogauss_threshold_"
+    ext = ".png"
+
+    for i in range(1, 11):
+        j = ((2 * i) - 1)
+        rev, thresh1 = cv2.threshold(img, 0, 255,
+                                     cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        status += cv2.imwrite(nogauss + str(i) + ext, thresh1)
+        img = cv2.GaussianBlur(img, (j, j), 0)
+        thresh1 = cv2.adaptiveThreshold(
+            img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 7, 2)
+        status += cv2.imwrite(gauss + str(i) + ext, thresh1)
+        rev, thresh1 = cv2.threshold(img, 0, 255,
+                                     cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        status += cv2.imwrite(thresh + str(i) + ext, thresh1)
+
     print("Image written to file-system : ", status)
